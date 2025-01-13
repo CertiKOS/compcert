95e8c11b543767c7ce655ed5f385a6de751ce6d4
Justin Restivo <justin@restivo.me>
2025-01-12 22:47:07 -0500

    fix: any translation error is now hard

--- driver/Driver.ml
+++ driver/Driver.ml
@@ -82,7 +82,7 @@ let compile_c_file sourcename ifile ofile =
     (Compiler.print_r_program !sym_mapping !composite_mapping module_name csyntax)
   with
   | Errors.OK _rprog -> printf "translated!"
-  | Errors.Error msg -> printf "error! %s" (C2C.string_of_errmsg msg)
+  | Errors.Error msg -> fatal_error no_loc "error! %s" (C2C.string_of_errmsg msg);
   ;
   (* (1) call out to transf_rust_program *)
   (* (2)  *)
