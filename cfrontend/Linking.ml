837690a2a82fccadffffb2263f1eb3d4540a2d8d
Justin Restivo <justin@restivo.me>
2025-04-26 11:56:04 -0400

    feat: linking works

--- cfrontend/Linking.ml
+++ cfrontend/Linking.ml
@@ -72,6 +72,15 @@ module IdentSet =
     let compare = Stdlib.compare
   end)
 
+type tyuid = (string * ident)
+
+module TySet =
+  Set.Make
+  (struct
+    type t = (string * ident)
+    let compare = Stdlib.compare
+  end)
+
 
 (* strings -> module name *)
 module Linking : sig
@@ -89,18 +98,16 @@ module Linking : sig
   (* ident: we have a representative "compatible" type for each type. The rest of the types are type aliases to it. *)
   (* val get_rep_type: ty_id: ident -> ident option *)
 
-  (* ty_id -> (mod_name, defn)*)
-  val get_type_definition: t -> ty_id: ident -> (string * composite_definition)
+  (* (ty_id, mod_name) -> defn *)
+  val get_type_definition: t -> ty_id: tyuid -> composite_definition
 
-  val get_rep_type_definition: t -> ty_id: ident -> (string * composite_definition)
+  val get_rep_type_definition: t -> ty_id: tyuid -> (tyuid * composite_definition)
 
-  val types_are_compat: t -> ty_1: composite_definition -> ty_2: composite_definition -> bool
+  val types_are_compat: t -> mod_1: string -> ty_1: composite_definition -> mod_2: string -> ty_2: composite_definition -> bool
 
   (* fills out r_ty_map s.t. there's only one "representative type" and the rest are de-duplicated *)
   val fill_out_rep_types: t -> unit
 
-
-  (**)
   val add_globdef: t -> name: ident -> mod_name: string -> defn: (Clight.coq_function Ctypes.fundef, Ctypes.coq_type) AST.globdef -> unit
 
   val add_ty_defn: t -> mod_name: string -> cd: composite_definition -> unit
@@ -109,14 +116,14 @@ end =
     type t = {
       (* type map: type ident -> (mod_name, composite_definition) *)
       (* *even* if the same name is. *)
-      type_map : (ident, string * composite_definition) Hashtbl.t;
+      type_map : (tyuid, composite_definition) Hashtbl.t;
 
       globvars : (ident, (string * (Clight.coq_function Ctypes.fundef, Ctypes.coq_type) AST.globdef)) Hashtbl.t;
 
       (* map of representative types *)
-      r_ty_map: (ident, ident) Hashtbl.t;
+      r_ty_map: (tyuid, tyuid) Hashtbl.t;
 
-      rep_types: (ident, IdentSet.t) Hashtbl.t;
+      rep_types: (tyuid, TySet.t) Hashtbl.t;
 
       (* there's two phases: build up types and globals, then resolve the globals to generate imports and representative types *)
       mutable is_locked: bool
@@ -142,35 +149,32 @@ end =
 
     let dump_rep_ty_table (state: t) unit =
       Printf.printf "Begininning dump r_ty_map: \n";
-      Hashtbl.iter (fun k v -> Printf.printf "\t %s (%ld) -> %s (%ld)\n" (ident_to_string state ~name:k) (k |> P.to_int32) (ident_to_string state ~name:v) (v |> P.to_int32)) state.r_ty_map;
+      Hashtbl.iter (fun (m_1, ty_1) (m_2, ty_2) -> Printf.printf "\t module %s, ty %s (%ld) -> module %s, ty %s (%ld)\n" (ident_to_string state ~name:ty_1) m_1 (ty_1 |> P.to_int32) m_2 (ident_to_string state ~name:ty_2) (ty_2 |> P.to_int32)) state.r_ty_map;
       Printf.printf "Ending dump r_ty_map: \n";
       flush stdout
 
     let dump_tylist (state: t) unit =
       Printf.printf "Begininning dump type_map: \n";
-      Hashtbl.iter (fun k v -> Printf.printf "%s (%ld)\n" (ident_to_string state ~name:k) (k |> P.to_int32)) state.type_map;
+      Hashtbl.iter (fun (m, ty_uid) v -> Printf.printf "mod %s, ty %s (%ld)\n" m (ident_to_string state ~name:ty_uid) (ty_uid |> P.to_int32)) state.type_map;
       Printf.printf "Ending dump type_map: \n";
       flush stdout
 
-    let print_rep_set s =
-      IdentSet.fold (fun s acc -> Printf.sprintf "%s %ld," acc (s |> P.to_int32)) s ""
+    let print_rep_set (state: t) (s: TySet.t) =
+      TySet.fold (fun (m, ty_uid) acc -> Printf.sprintf "%s (%s, %s (%ld))," acc m (ident_to_string state ~name:ty_uid) (ty_uid |> P.to_int32)) s ""
 
     let dump_rep_types (state: t) =
       Printf.printf "Begininning dump rep_types: \n";
-      Hashtbl.iter (fun k s ->
-        Printf.printf "\t %s (%ld) -> {%s}\n" (ident_to_string state ~name:k) (k |> P.to_int32) (print_rep_set s)
+      Hashtbl.iter (fun (m, ty_uid) s ->
+        Printf.printf "\t (%s, %s (%ld)) -> {%s}\n" m (ident_to_string state ~name:ty_uid) (ty_uid |> P.to_int32) (print_rep_set state s)
     ) state.rep_types;
       Printf.printf "ending dump rep_types: \n";
       flush stdout
-
-
-
 
     let add_globdef (state: t) ~name ~mod_name ~defn = Hashtbl.replace state.globvars name (mod_name, defn)
 
     let add_ty_defn (state: t) ~mod_name ~cd =
       let Composite(id, _, _, _) = cd in
-      Hashtbl.add state.type_map id (mod_name, cd)
+      Hashtbl.add state.type_map (mod_name, id) cd
 
     (* TODO this is a HACK. Really this information should be propagated through the parser *)
     let is_anon_ident (state: t) ~name =
@@ -187,29 +191,32 @@ end =
       in
       len >= 2 && String.get name_string 0 = '_' && check_digits 1
 
-    let rec rep_ty_equal (state: t) ~cty_1 ~cty_2 =
+    let rec rep_ty_equal (state: t) ~mod_1 ~cty_1 ~mod_2 ~cty_2 =
       match (cty_1, cty_2) with
       | (Tstruct(i', a'), Tstruct(j', b'))
       | (Tunion(i', a'), Tunion(j', b')) ->(
           if i' = j' then true
           else
-            match (Hashtbl.find_opt state.r_ty_map i', Hashtbl.find_opt state.r_ty_map j') with
+            (* the struct should 100% exist in the module *)
+            (* otherwise how will it know that struct exists?? *)
+            match (Hashtbl.find_opt state.r_ty_map (mod_1, i'), Hashtbl.find_opt state.r_ty_map (mod_2, j')) with
             | (Some(i''), Some(j'')) -> i'' = j''
             | _ -> false)
       | (Tfunction(tl, rty, _cc), Tfunction(tl', rty', _cc')) ->
-          rep_tylist_equal state tl tl' && rep_ty_equal state ~cty_1:rty ~cty_2:rty' && _cc = _cc'
+          rep_tylist_equal state ~mod_1 tl ~mod_2 tl' && rep_ty_equal state ~mod_1 ~cty_1:rty ~mod_2 ~cty_2:rty' && _cc = _cc'
       | (Tpointer(ty, _cc), Tpointer(ty', _cc')) ->
-          rep_ty_equal state ~cty_1:ty ~cty_2:ty'
-      | (Tarray(ty, num, _cc), Tarray(ty', num', _cc')) -> rep_ty_equal state ~cty_1:ty ~cty_2:ty' && num = num' && _cc = _cc'
+          rep_ty_equal state ~mod_1 ~cty_1:ty ~mod_2 ~cty_2:ty'
+      | (Tarray(ty, num, _cc), Tarray(ty', num', _cc')) -> rep_ty_equal state ~mod_1 ~cty_1:ty ~mod_2 ~cty_2:ty' && num = num' && _cc = _cc'
       | _ -> cty_1 = cty_2
-    and rep_tylist_equal (state: t) tl1 tl2 =
+    and rep_tylist_equal (state: t) ~mod_1 tl1 ~mod_2 tl2 =
       match (tl1, tl2) with
       | (Tcons(ty1, tl1'), Tcons(ty2, tl2')) ->
-          rep_ty_equal state ~cty_1:ty1 ~cty_2:ty2 && rep_tylist_equal state tl1' tl2'
+          rep_ty_equal state ~mod_1 ~cty_1:ty1 ~mod_2 ~cty_2:ty2 && rep_tylist_equal state ~mod_1 tl1' ~mod_2 tl2'
       | (Tnil, Tnil) -> true
       | _ -> false
 
-    let types_are_compat (state: t) ~ty_1 ~ty_2 =
+    (* module arguments are provided because ty_map needs those for lookup *)
+    let types_are_compat (state: t) ~mod_1 ~ty_1 ~mod_2 ~ty_2 =
       match ty_1,ty_2 with
       | Ctypes.Composite (_, sou, mems, attrs), Ctypes.Composite(_, sou_, mems_, attrs_) -> (
           let sou_r =
@@ -218,9 +225,9 @@ end =
             | (Ctypes.Struct, Ctypes.Struct) -> true
             | _ -> false
           in
-          let mem_eq_fn = fun m_1 m_2 -> (
-              match (m_1, m_2) with
-              | Ctypes.Member_plain(_, ty), Ctypes.Member_plain(_, ty') -> rep_ty_equal state ~cty_1:ty ~cty_2:ty'
+          let mem_eq_fn = fun mems_1 mems_2 -> (
+              match (mems_1, mems_2) with
+              | Ctypes.Member_plain(_, ty), Ctypes.Member_plain(_, ty') -> rep_ty_equal state ~cty_1:ty ~cty_2:ty' ~mod_1 ~mod_2
               | Ctypes.Member_bitfield(_, a, b, c, d, e), Ctypes.Member_bitfield(_, a', b', c', d', e') ->
                 a = a' && b = b' && c = c' && d = d' && e = e'
               | _ -> false
@@ -241,51 +248,55 @@ end =
     let get_type_definition (state: t) ~ty_id = Hashtbl.find state.type_map ty_id
 
     let get_rep_type_definition (state: t) ~ty_id =
-      get_type_definition state ~ty_id:(Hashtbl.find state.r_ty_map ty_id )
+      let rep_ty = Hashtbl.find state.r_ty_map ty_id in
+      let cd = get_type_definition state ~ty_id:(rep_ty) in
+      (rep_ty, cd)
 
     (* TODO the way to handle mutual recursion is a stack. But, we don't yet handle that *)
     (* need to make a test case then handle it. *)
-    let rec internal_has_rep_type (state: t) (ty_1: composite_definition) (l: composite_definition list) =
+    let rec internal_has_rep_type (state: t) (mod_1: string) (ty_1: composite_definition) (l: (string * composite_definition) list) =
       match l with
-      | (Composite(id, _, _, _) as ty_2) :: l' ->
-          if types_are_compat state ~ty_1 ~ty_2 then
-            Some(id)
-          else internal_has_rep_type state ty_1 l'
+      | (mod_2, (Composite(id, _, _, _) as ty_2)) :: l' ->
+          if types_are_compat state ~mod_1 ~ty_1 ~mod_2 ~ty_2 then
+            Some(mod_2, id)
+          else internal_has_rep_type state mod_1 ty_1 l'
       | nil -> None
 
 
-    let has_rep_type (state: t) (cd: composite_definition) =
-      let ty_list = Hashtbl.to_seq_keys state.rep_types |> List.of_seq |> List.map (fun x -> Hashtbl.find state.type_map x |> snd)
-      in internal_has_rep_type state cd ty_list
+    let has_rep_type (state: t) (mod_name: string) (cd: composite_definition) : tyuid option =
+      let ty_list = Hashtbl.to_seq_keys state.rep_types |> List.of_seq |> List.map (fun x -> (fst x, Hashtbl.find state.type_map x))
+      in internal_has_rep_type state mod_name cd ty_list
 
-    (* if it's contained: great *)
-    (* if it's not contained: add as a representative type *)
-    let rec internal_fill_out_rep_types_initial (state: t) (l: (ident * (string * composite_definition))list) =
+    (* we start out with the internal representation not filled out *)
+    (* and we gradually fill it out. *)
+    (* if it's contained: great, keep it contained *)
+    (* if it's not contained: add as a representative type of itself *)
+    let rec internal_fill_out_rep_types_initial (state: t) (l: (tyuid * composite_definition) list) =
       match l with
-      | (id, (mod_name, Composite(_, sou, mems, attrs) as cd)) :: l' ->
-          (match has_rep_type state (snd cd) with
+      | ((mod_name, id) as typ_uid, (Composite(_, sou, mems, attrs) as cd)) :: l' ->
+          (match has_rep_type state mod_name cd with
           | None -> (
-            Hashtbl.add state.rep_types id (IdentSet.singleton id);
-            Hashtbl.add state.r_ty_map id id;
+            Hashtbl.add state.rep_types typ_uid (TySet.singleton typ_uid);
+            Hashtbl.add state.r_ty_map typ_uid typ_uid;
           )
           | Some(rep_id) -> (
             let old_set = Hashtbl.find state.rep_types rep_id in
-            let new_set = IdentSet.add id old_set in
+            let new_set = TySet.add typ_uid old_set in
             Hashtbl.replace state.rep_types rep_id new_set;
-            Hashtbl.replace state.r_ty_map id rep_id;
+            Hashtbl.replace state.r_ty_map typ_uid rep_id;
           ));
           internal_fill_out_rep_types_initial state l'
       | nil -> ()
 
-    let rec internal_check_rep_types (state: t) (tid: ident) (l: ident list) =
+    let rec internal_check_rep_types (state: t) (tid: tyuid) (l: tyuid list) =
       dump_rep_ty_table state ();
       dump_rep_types state;
 
       match l with
       | tid2 :: l' -> (
-          let cd1 = Hashtbl.find state.type_map tid |> snd in
-          let cd2 = Hashtbl.find state.type_map tid2 |> snd in
-          if types_are_compat state ~ty_1:cd1 ~ty_2:cd2 then(
+          let cd1 = Hashtbl.find state.type_map tid in
+          let cd2 = Hashtbl.find state.type_map tid2 in
+          if types_are_compat state ~mod_1:(fst tid) ~mod_2:(fst tid2) ~ty_1:cd1 ~ty_2:cd2 then(
             Hashtbl.replace state.r_ty_map tid tid2;
 
             let tid_rep_eles = Hashtbl.find state.rep_types tid in
@@ -295,12 +306,12 @@ end =
             Hashtbl.remove state.rep_types tid;
             Printf.printf"after removal \n";
             dump_rep_types state;
-            Printf.printf "TID1 %s %ld, TID2 is %s %ld \n" (ident_to_string state ~name:tid) (tid |> P.to_int32) (ident_to_string state ~name:tid2) (tid2 |> P.to_int32);
+            Printf.printf "TID1 %s %ld, TID2 is %s %ld \n" (ident_to_string state ~name:(snd tid)) (tid |> snd |> P.to_int32) (ident_to_string state ~name:(snd tid2)) (snd tid2 |> P.to_int32);
 
             flush stdout;
 
             let tid2_rep_eles = Hashtbl.find state.rep_types tid2 in
-            Hashtbl.replace state.rep_types tid2 (IdentSet.union tid_rep_eles tid2_rep_eles);
+            Hashtbl.replace state.rep_types tid2 (TySet.union tid_rep_eles tid2_rep_eles);
             true
             (* type no longer exists so return out of here*)
           )
@@ -311,7 +322,7 @@ end =
       | nil -> false
 
 
-    let rec internal_fill_out_rep_types_rest (state: t) (made_changes: bool) (l: ident list) =
+    let rec internal_fill_out_rep_types_rest (state: t) (made_changes: bool) (l: tyuid list) =
       match l with
       | tid :: l' ->
           let more_changes = internal_check_rep_types state tid l' in
